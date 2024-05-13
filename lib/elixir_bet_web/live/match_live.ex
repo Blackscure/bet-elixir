4cf4b266aa9073b4d9864e775251e64d47ebd874
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-05-13 16:32:41 +0300

    home and away team pulled in matches table

--- lib/elixir_bet_web/live/match_live.ex
+++ lib/elixir_bet_web/live/match_live.ex
@@ -1,10 +1,24 @@
 defmodule ElixirBetWeb.MatchLive do
   use ElixirBetWeb, :live_view
 
+  alias ElixirBet.{Teams, Repo}
+
   def mount(_params, _session, socket) do
     matches = ElixirBet.Repo.all(ElixirBet.Matches.Match)
     {:ok, assign(socket, matches: matches)}
   end
 
+  def get_home_team(home_team_id) do
+    case Repo.get(Teams.Team, home_team_id) do
+      nil -> "Unknown team"
+      team -> team.name
+    end
+  end
 
+  def get_away_team(away_team_id) do
+    case Repo.get(Teams.Team, away_team_id) do
+      nil -> "Unknown team"
+      team -> team.name
+    end
+  end
 end
