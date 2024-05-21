e12e991ac4f5008fbcfb10481e7e8b56a299a075
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-05-21 16:52:35 +0300

    s

--- lib/elixir_bet_web/live/dashboard_live.ex
+++ lib/elixir_bet_web/live/dashboard_live.ex
@@ -1,3 +1,27 @@
 defmodule ElixirBetWeb.DashboardLive do
   use ElixirBetWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    user_count = fetch_user_count()
+    match_count = fetch_match_count()
+    league_count = fetch_league_count()
+
+    {:ok, assign(socket, user_count: user_count, match_count: match_count, league_count: league_count)}
+  end
+
+  defp fetch_user_count() do
+    count = ElixirBet.Repo.aggregate(ElixirBet.Accounts.User, :count, :id)
+    count
+  end
+
+  defp fetch_match_count() do
+    count = ElixirBet.Repo.aggregate(ElixirBet.Matches.Match, :count, :id)
+    count
+  end
+
+  defp fetch_league_count() do
+    count = ElixirBet.Repo.aggregate(ElixirBet.Leagues.League, :count, :id)
+    count
+  end
+
 end
